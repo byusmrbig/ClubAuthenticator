43cfd85f112edc13f464739d1300c6a293c1a2a1
Nate Baker <natesbaker@gmail.com>
2015-10-27 10:11:16 -0700

    Cleaned comments, wiped the log file from last project's logs, finished updating the old code for the new project, and updated the README.

--- authenticator.py
+++ authenticator.py
@@ -23,7 +23,7 @@ class authenticator:
 		cleanID += cardID[6:10]	# append digits 6 to 9 of the card input onto the sanitized input
 		return cleanID	# return the sanitized student ID in the form of **-***-****
 
-	def cardAuthCheck(self, sanitizedCardID):	# checks the membership status of the swiping student
+	def cardStatusCheck(self, sanitizedCardID):	# checks the membership status of the swiping student
 		nonAgreement = file('needs_agreement.txt')	# pulls members who haven't signed agreements from needs_agreement.txt
 		membersFile = file('full_member.txt')	# pulls full members from full_member.txt
 		needsDues = self.PAY_DUES	# automatically sets status to 'Student Needs to Pay Dues'
@@ -46,33 +46,25 @@ class authenticator:
 		GPIO.setup(self.blueLED, GPIO.OUT)	# set the LED port to output
 		GPIO.setup(self.redLED, GPIO.OUT)	# set the LED port to output
 
-	# def keypadResult:
-		# check GPIO ports for input character, and get them one at a time
-		# set timer for time entered function (30 or so seconds, after users has swipped; exits and returns null)
-		# set timer for time since last key stroke (5 or so seconds; exits if user doesn't keep hitting keys)
-		# 
-	
-	# def getKeys(self):
-		# giant elif chain that listens for keys and returns a single char
-
 	def main(self):	# the main function
 		signal.signal(signal.SIGINT, signal_handler)	# prepare to handle keyboard interrupts
 		while(1):	# run access input indefinitely
 			self.GPIOprep()	# prep the GPIO
 			userInput = raw_input()	# wait for the user to swipe their card
 			cleanedSwipeInput = self.cleanInput(userInput)	# convert the card input into a human-readable input
-			swipeResult = self.cardAuthCheck(cleanedSwipeInput)	# determine if person swipping card is authorized to enter
+			swipeResult = self.cardStatusCheck(cleanedSwipeInput)	# determine club status of the person swipping their card
 			self.swipeLog(cleanedSwipeInput, swipeResult)	# log the card swipe
-			if swipeResult == 'Denied':
+			if swipeResult == PAY_DUES:	# if the user needs to pay their dues
+				# signal the LED to red for two seconds
 				# break, restarting the while loop
 				continue
-			elif swipeResult == 'Granted':
-				# keypadResult = self.getKeypadInputs()
-				GPIO.output(self.led, 1)
-				time.sleep(3)
-				GPIO.output(self.led, 0)
-			elif swipeResult == 'Admin Use':
-				# begin superUserPin function
+			elif swipeResult == AGREEMENT_NEEDED:	# if the user needs to sign the agreement
+				# signal the LED to blue for 2 seconds
+				# break, restarting the while loop
+				continue
+			elif swipeResult == FULL_MEMBER:
+				# signal the LED to green for 2 seconds
+				# break, restarting the while loop
 				continue
 
 if __name__ == '__main__':	# Prepare the above class to be used in order to implement object-oriented programming
